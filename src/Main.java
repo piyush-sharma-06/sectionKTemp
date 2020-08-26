a5ac79b08085f3b5b77aeed2c228558ed94835ca
piyush <piyush.sharma_cs19@gla.ac.in>
2020-08-26 19:51:30 +0530

    add the main method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Piyush Sharma (sec - k)
  *  Date: 22/08/20
  *  Time: 3:07 PM
  *  File Name : Main.java
  * */
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
